7717de77fa888ae695c0e6fc2d6043f89d0ad5fc
Thomas Weideman <thomas.weideman@gmail.com>
2021-03-04 21:32:53 +0000

    Update getAllReservations with SQL query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -68,7 +68,12 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT * FROM reservations
+  WHERE guest_id = $1
+  LIMIT $2
+  `, [guest_id, limit])
+  .then(res => res.rows);
 }
 exports.getAllReservations = getAllReservations;
 
